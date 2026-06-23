d0cd31fca063aa7121832c28f4d31da217d3b5a5
Jose Gomez <gomez1993jose@gmail.com>
2026-06-23 14:04:29 +0800

    Disable Firefox configuration in playwright.config.ts
    Comment out the Firefox configuration in Playwright.

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
